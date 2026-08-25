8c43d20c458918dc04f154070e1ef0b6563a5c4b
Jayygotnochills <jaypurswani6@gmail.com>
2026-08-25 20:22:27 +0530

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const base_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: base_URL,
 })
 
 export default api
